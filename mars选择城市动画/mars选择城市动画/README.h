6dd39a0eb71985460f72af97ed386709e110e44f
LZS-bobo <luozhuangshen@luozhuangshendeMacBook-Pro.local>
2017-02-06 11:39:58 +0800

    README

---
+++ mars选择城市动画/mars选择城市动画/README.h
@@ -0,0 +1,21 @@
+//
+//  README.h
+//  mars选择城市动画
+//
+//  Created by 罗壮燊 on 2017/2/6.
+//  Copyright © 2017年 罗壮燊. All rights reserved.
+//
+
+
+/*
+ # 分析动画组成
+ - 3D旋转
+    * CATransform3DRotate
+ - 水平移动
+    * transform.translation.x
+ - 缩小
+    * transform.scale
+ # 动画实现
+ - 利用iOS 核心动画 
+ 
+*/
--- mars选择城市动画/mars选择城市动画/SBCustomTransition.m
+++ mars选择城市动画/mars选择城市动画/SBCustomTransition.m
@@ -21,11 +21,17 @@ double radianFromDegree(float degrees) {
 
 void animation(UIView *view, SBDirection direction, BOOL isPush)
 {
+    //创建组动画
     CAAnimationGroup *animaGroup = [CAAnimationGroup animation];
     animaGroup.duration = 0.5;
+    
+    //视图在屏幕的方向
     BOOL isLeft = (direction == SBDirectionLeft);
     
     CATransform3D transform = CATransform3DIdentity;
+    
+    //m34 http://www.cnblogs.com/OIMM/p/5205528.html
+    //m43 z轴平移 由于两个view做m34动画的时候会出现重叠 调整两个视图的z轴距离避免重叠
     transform.m34 = -1.0/ 500;
     if (isPush) {
         transform.m43 = isLeft ? 1000:0;
@@ -33,6 +39,7 @@ void animation(UIView *view, SBDirection direction, BOOL isPush)
         transform.m43 = isLeft ? 0:1000;
     }
     
+    //CATransform3D 帧动画
     CAKeyframeAnimation *anim_3d =  [CAKeyframeAnimation animationWithKeyPath:@"transform"];
     
     CATransform3D begin = CATransform3DRotate(transform, radianFromDegree(0), 0, 1, 0);
@@ -50,10 +57,11 @@ void animation(UIView *view, SBDirection direction, BOOL isPush)
     
     anim_3d.values = @[[NSValue valueWithCATransform3D: begin],[NSValue valueWithCATransform3D:middle] ,[NSValue valueWithCATransform3D:end]];
     
-    
+    //平移 帧动画
     CAKeyframeAnimation *anim_move =  [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.x"];
     anim_move.values = @[@(0),@(isLeft ? -50:50),@(isLeft ? -20:0)];
     
+    //缩放 帧动画
     CAKeyframeAnimation *anim_scale =  [CAKeyframeAnimation animationWithKeyPath:@"transform.scale"];
     anim_scale.values = @[@(1),@(0.6),@(isLeft ? 0.8:1)];
     
@@ -77,11 +85,13 @@ void animation(UIView *view, SBDirection direction, BOOL isPush)
     
     [containerView addSubview:toVC.view];
     
+    //设置锚点和position 使得旋转位置在左边
     fromVC.view.layer.position = CGPointMake(0, fromVC.view.frame.size.height/2.0f);
     fromVC.view.layer.anchorPoint = CGPointMake(0, 0.5);
     
-    
     toVC.view.frame = [transitionContext finalFrameForViewController:toVC];
+    
+    //设置锚点和position 使得旋转位置在右边
     toVC.view.layer.position = CGPointMake(toVC.view.frame.size.width, toVC.view.frame.size.height/2.0f);
     toVC.view.layer.anchorPoint = CGPointMake(1, 0.5);
     
@@ -90,6 +100,7 @@ void animation(UIView *view, SBDirection direction, BOOL isPush)
     animation(fromVC.view, SBDirectionLeft, YES);
     animation(toVC.view, SBDirectionRight, YES);
     
+    //结束转场动画
     [self performSelector:@selector(finishAnim:) withObject:transitionContext afterDelay:0.5];
     
 }
